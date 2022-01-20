f837f9f6bf71a0331a39e9c114da4e04510f0cd1
Hasan Iqbal <swapniliqbal@gmail.com>
2022-01-20 13:26:41 -0500

    Reload page after data insertion

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -40,7 +40,7 @@ indexRoute.get("/", (req, res) => {
     });
 });
 
-// Clear data
+// Delete data
 indexRoute.post("/delete_original", (req, res) => {
     // When the user clicks the clear button, delete all the data from the database
     const connection = mysql.createConnection({
@@ -75,33 +75,58 @@ indexRoute.post("/insert_original", (req, res) => {
     const rawData = fs.readFileSync('./given_files/marketingDataFixed.json')
     let json_file = JSON.parse(rawData);
 
-    insertFileFromData(json_file);
-    res.redirect("/");
+    // Check if any data exists in the database. If so, show a window.alert to the user
+    connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+
+        // Only insert the original data if there is no data in the database
+        if (rows.length > 0) {
+            console.log("There is already data in the database. Please delete it first.");
+            res.redirect("/");
+        } else {
+            // Insert the data into the database
+            let dataArray = [];
+
+            // Loop through the json file and insert each week into the database
+            Object.keys(json_file.marketingData).forEach(key => {
+                const weekNumber = parseInt(key.split("week")[1]);
+                const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+                const webVisitors = json_file.marketingData[key].webVisitors;
+                const prClippings = json_file.marketingData[key].prClippings;
+
+                dataArray.push([weekNumber, dateCreated, webVisitors, prClippings]);
+            });
+
+            let sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
+            connection.query(sql, [dataArray], (error, results, fields) => {
+                if (error) {
+                    console.log(error);
+                    return res.send(error);
+                }
+            }).on("end", () => {
+                res.redirect("/");
+            })
+        }
+    })
 })
 
-const insertFileFromData = (json_file) => {
+// const insertFileFromData = (json_file) => {
 
-    const connection = mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: DB_NAME
-    });
+//     const connection = mysql.createConnection({
+//         host: DB_HOST,
+//         user: DB_USERNAME,
+//         password: DB_PASSWORD,
+//         database: DB_NAME
+//     });
 
-    // Loop through the json file and insert each week into the database
-    Object.keys(json_file.marketingData).forEach(key => {
-        const weekNumber = parseInt(key.split("week")[1]);
-        const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
-        const webVisitors = json_file.marketingData[key].webVisitors;
-        const prClippings = json_file.marketingData[key].prClippings;
 
-        connection.query(`INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ('${weekNumber}', '${dateCreated}', ${webVisitors}, ${prClippings})`, (err, rows, fields) => {
-            if (err) {
-                console.log(err);
-                return res.send(err);
-            }
-        });
-    });
-}
+// }
+
+indexRoute.post("/insert_own", (req, res) => {
+    // First delete all data from DB
+})
 
 module.exports = indexRoute;
