ff18af879fce28821fddae48fce8394842b63243
Hasan Iqbal <swapniliqbal@gmail.com>
2022-01-20 03:46:47 -0500

    Added features: graph analytics, clear the table, revert back to original json file

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const DB_HOST = process.env.DB_HOST;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_NAME = process.env.DB_NAME;
 const PORT = process.env.PORT || 3000;
 
 // Parsing Middlewares
@@ -30,42 +26,15 @@ app.use("/js", express.static(__dirname + "public/js"))
 // Routes
 const indexRoute = require("./src/routes/indexRoute");
 app.get("/", indexRoute)
+app.post("/delete_original", indexRoute)
+app.post("/insert_original", indexRoute)
+app.post("/insert_own", indexRoute)
+
 
 // 404 Page
 app.get("*", (req, res) => {
     res.render("404")
 })
-
-// Read marketing data
-let rawdata = fs.readFileSync('./given_files/marketingDataFixed.json');
-let json_file = JSON.parse(rawdata);
-
-// Add marketing data to database
-const insertIntoDatabase = (json_file) => {
-    const connection = mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: DB_NAME
-    });
-
-    // Loop through the json file and insert each week into the database
-    Object.keys(json_file.marketingData).forEach(key => {
-        const weekNumber = parseInt(key.split("week")[1]);
-        const dateCreated = json_file.marketingData[key].dateCreated;
-        const webVisitors = json_file.marketingData[key].webVisitors;
-        const prClippings = json_file.marketingData[key].prClippings;
-        
-        connection.query(`INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ('${weekNumber}', '${dateCreated}', ${webVisitors}, ${prClippings})`, (err, rows, fields) => {
-            if (err) {
-                console.log(err);
-                return res.send(err);
-            }
-        });
-    });
-}
-
-// insertIntoDatabase(json_file);
 
 // Listen
 app.listen(PORT, (error) => {
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -5,7 +5,8 @@ const mysql = require("mysql");
 const fs = require("fs");
 
 // Environment Variables
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const e = require("express");
 dotenv.config();
 
 const DB_HOST = process.env.DB_HOST;
@@ -32,12 +33,75 @@ indexRoute.get("/", (req, res) => {
             return res.send(err);
         }
 
-        console.log(rows);
         res.render("index", {
             title: "Marketing Data",
             marketingData: rows
         });
     });
 });
+
+// Clear data
+indexRoute.post("/delete_original", (req, res) => {
+    // When the user clicks the clear button, delete all the data from the database
+    const connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    });
+
+    connection.query('DELETE FROM marketingdata', (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+
+        console.log("All data has been deleted from the database");
+        res.redirect("/");
+    });
+})
+
+// Re-insert the original data
+indexRoute.post("/insert_original", (req, res) => {
+
+    const connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    });
+
+    // Read marketing data
+    const rawData = fs.readFileSync('./given_files/marketingDataFixed.json')
+    let json_file = JSON.parse(rawData);
+
+    insertFileFromData(json_file);
+    res.redirect("/");
+})
+
+const insertFileFromData = (json_file) => {
+
+    const connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USERNAME,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    });
+
+    // Loop through the json file and insert each week into the database
+    Object.keys(json_file.marketingData).forEach(key => {
+        const weekNumber = parseInt(key.split("week")[1]);
+        const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+        const webVisitors = json_file.marketingData[key].webVisitors;
+        const prClippings = json_file.marketingData[key].prClippings;
+
+        connection.query(`INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ('${weekNumber}', '${dateCreated}', ${webVisitors}, ${prClippings})`, (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+        });
+    });
+}
 
 module.exports = indexRoute;
