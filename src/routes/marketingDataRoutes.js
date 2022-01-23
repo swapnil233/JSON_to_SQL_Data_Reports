5296f5be2f88b7db18c99da8c1d8761e569b2871
Hasan Iqbal <swapniliqbal@gmail.com>
2022-01-22 20:46:25 -0500

    Inserting custom JSON marketing data

--- src/routes/marketingDataRoutes.js
+++ src/routes/marketingDataRoutes.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const router = express.Router();
+const path = require("path")
 
 // MySQL
 const mysql = require("mysql");
@@ -13,6 +15,13 @@ const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Middlewares
+router.use(express.urlencoded({
+    extended: true
+}));
+router.use(express.json());
+router.use(fileUpload())
+
 const DB_HOST = process.env.DB_HOST;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
@@ -20,23 +29,30 @@ const DB_NAME = process.env.DB_NAME;
 
 router.get("/", (req, res) => {
 
-    const connection = mysql.createConnection({
+    const pool = mysql.createPool({
+        connectionLimit: 10,
         host: DB_HOST,
         user: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_NAME
     });
 
-    // Retrieve everything from marketingdata table and render it into the page
-    connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
-        if (err) {
-            console.log(err);
-            return res.send(err);
-        }
+    pool.getConnection((err, connection) => {
+        // Retrieve everything from marketingdata table and render it into the page
+        connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+            // Release the connection
+            connection.release();
+            console.log("Connection released!");
 
-        res.render("marketingData", {
-            title: "Marketing Data",
-            marketingData: rows
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+
+            res.render("marketingData", {
+                title: "Marketing Data",
+                marketingData: rows
+            });
         });
     });
 });
@@ -44,28 +60,34 @@ router.get("/", (req, res) => {
 // Delete all table data
 router.post("/delete_original", (req, res) => {
 
-    const connection = mysql.createConnection({
+    const pool = mysql.createPool({
+        connectionLimit: 10,
         host: DB_HOST,
         user: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_NAME
     });
 
-    connection.query('DELETE FROM marketingdata', (err, rows, fields) => {
-        if (err) {
-            console.log(err);
-            return res.send(err);
-        }
+    pool.getConnection((err, connection) => {
+        connection.query('DELETE FROM marketingdata', (err, rows, fields) => {
+            connection.release();
+            console.log("Deleted all data from marketingdata table & released connection.");
 
-        console.log("All data has been deleted from the database");
-        res.redirect("/");
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+
+            res.redirect("/");
+        });
     });
 })
 
 // Re-insert the original (given) data
 router.post("/insert_original", (req, res) => {
 
-    const connection = mysql.createConnection({
+    const pool = mysql.createPool({
+        connectionLimit: 10,
         host: DB_HOST,
         user: DB_USERNAME,
         password: DB_PASSWORD,
@@ -76,50 +98,150 @@ router.post("/insert_original", (req, res) => {
     const rawData = fs.readFileSync('./given_files/marketingDataFixed.json')
     let json_file = JSON.parse(rawData);
 
-    // Don't write any data if table isn't empty
-    connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
-        if (err) {
-            console.log(err);
-            return res.send(err);
-        }
+    pool.getConnection((err, connection) => {
+        // Don't write any data if table isn't empty
+        connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
 
-        // Only insert the original data if there is no data in the database
-        if (rows.length > 0) {
-            console.log("There is already data in the database. Please delete it first.");
-            res.redirect("/");
-        } else {
+            // Only insert the original data if there is no data in the database
+            if (rows.length > 0) {
+                connection.release();
+                res.send("The table is not empty. Please clear all data first.");
+            } else {
+                // Data array, to be inserted into table via SQL
+                let dataArray = [];
 
-            // Data array, to be inserted into table via SQL
-            let dataArray = [];
+                // Loop through the json file and insert each week's data into the database
+                Object.keys(json_file.marketingData).forEach(key => {
+                    const weekNumber = parseInt(key.split("week")[1]);
+                    const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+                    const webVisitors = json_file.marketingData[key].webVisitors;
+                    const prClippings = json_file.marketingData[key].prClippings;
 
-            // Loop through the json file and insert each week's data into the database
-            Object.keys(json_file.marketingData).forEach(key => {
-                const weekNumber = parseInt(key.split("week")[1]);
-                const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
-                const webVisitors = json_file.marketingData[key].webVisitors;
-                const prClippings = json_file.marketingData[key].prClippings;
+                    dataArray.push([weekNumber, dateCreated, webVisitors, prClippings]);
+                });
 
-                dataArray.push([weekNumber, dateCreated, webVisitors, prClippings]);
-            });
+                // Insert into table
+                const sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
 
-            // Insert into table
-            const sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
-
-            connection.query(sql, [dataArray], (error, results, fields) => {
-                if (error) {
-                    console.log(error);
-                    return res.send(error);
-                }
-            }).on("end", () => {
-                res.redirect("/");
-            })
-        }
-    })
+                connection.query(sql, [dataArray], (error, results, fields) => {
+                    if (error) {
+                        console.log(error);
+                        return res.send(error);
+                    }
+                }).on("end", () => {
+                    connection.release();
+                    console.log("Inserted original data into marketingdata table & released connection.");
+                    res.redirect("/");
+                })
+            }
+        })
+    });
 })
 
 // Let users insert their own JSON file
-router.post("/insert_own", (req, res) => {
-    // First delete all data from DB
-})
+router.post('/upload', async (req, res) => {
+
+    // If no file uploaded
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).send("No files uploaded")
+    }
+
+    // If the uploaded file is not a JSON file, return error
+    if (req.files.marketingDataJSON.mimetype !== "application/json") {
+        res.status(400).send("You can only upload JSON files")
+    }
+
+    // If the uploaded json file is greater 4mb (the max json file size), return error
+    if (req.files.marketingDataJSON.size > 4000000) {
+        res.status(400).send("The file is too large. Please upload a smaller file.")
+    }
+
+    const uploadedJSONFile = req.files.marketingDataJSON;
+    const uploadPath = path.join(__dirname, "../../uploads/") + uploadedJSONFile.name;
+
+    // Put the uploaded file in /uploads folder
+    await uploadedJSONFile.mv(uploadPath, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+
+        // Read the uploaded JSON file
+        const rawData = fs.readFileSync(uploadPath)
+        let json_file = JSON.parse(rawData);
+
+        const pool = mysql.createPool({
+            connectionLimit: 10,
+            host: DB_HOST,
+            user: DB_USERNAME,
+            password: DB_PASSWORD,
+            database: DB_NAME
+        });
+
+        pool.getConnection((err, connection) => {
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+
+            // Don't write any data if table isn't empty
+            connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+                // Release the connection
+                connection.release();
+
+                if (err) {
+                    console.log(err);
+                    return res.send(err);
+                }
+
+                // Only insert the given data if there is no data in the database
+                if (rows.length > 0) {
+                    console.log("There is already data in the database. Deleting it now...");
+                    // Delete all the data in marketingdata table
+                    connection.query('DELETE FROM marketingdata', (err, rows, fields) => {
+                        // Release the connection
+                        connection.release();
+
+                        if (err) {
+                            console.log(err);
+                            return res.send(err);
+                        }
+                    });
+                } else {
+                    // Data array, to be inserted into table via SQL
+                    let dataArray = [];
+
+                    // Loop through the json file and insert each week's data into the database
+                    Object.keys(json_file.marketingData).forEach(key => {
+                        const weekNumber = parseInt(key.split("week")[1]);
+                        const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+                        const webVisitors = json_file.marketingData[key].webVisitors;
+                        const prClippings = json_file.marketingData[key].prClippings;
+                        dataArray.push([weekNumber, dateCreated, webVisitors, prClippings]);
+                    });
+
+                    // Insert into table
+                    const sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
+
+                    connection.query(sql, [dataArray], (error, results, fields) => {
+                        // Release the connection
+                        connection.release();
+
+                        if (error) {
+                            console.log(error);
+                            return res.send(error);
+                        }
+                    }).on("end", () => {
+                        res.redirect("/");
+                    })
+                }
+            })
+        })
+    });
+});
 
 module.exports = router;
