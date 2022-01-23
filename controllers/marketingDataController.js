e971b74eb9b837e0a3a386739c240e7ea2410b45
Hasan Iqbal <swapniliqbal@gmail.com>
2022-01-23 00:23:05 -0500

    Created a controller for marketing data page's routes.

---
+++ controllers/marketingDataController.js
@@ -0,0 +1,253 @@
+const fileUpload = require("express-fileupload");
+const path = require("path")
+
+// Environment Variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+// MySQL
+const mysql = require("mysql");
+
+// MySQL Config
+const DB_HOST = process.env.DB_HOST;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+// File System
+const fs = require("fs");
+
+const pool = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME
+});
+
+// Index Route
+const marketingData_index = (req, res) => {
+    pool.getConnection((err, connection) => {
+        // Retrieve everything from marketingdata table and render it into the page
+        connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+            // Release the connection
+            connection.release();
+
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+
+            res.render("marketingData", {
+                title: "Marketing Data",
+                marketingData: rows
+            });
+        });
+    });
+};
+
+// Deleting data
+const marketingData_delete = (req, res) => {
+    pool.getConnection((err, connection) => {
+        connection.query('DELETE FROM marketingdata', (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+
+            console.log("Deleted all data from marketingdata table & released connection.");
+            connection.release();
+
+            res.redirect("/");
+        });
+    });
+}
+
+// Insert original/given marketing data
+const marketingData_insertOriginal = (req, res) => {
+    // Read the given JSON file
+    const rawData = fs.readFileSync('./given_files/marketingDataFixed.json')
+    let json_file = JSON.parse(rawData);
+
+    pool.getConnection((err, connection) => {
+        connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+                connection.release();
+                console.log("Error connecting to the DB. Connection released.")
+                return res.send(err);
+            }
+
+            // Only insert the original JSON data if the table is empty.
+            if (rows.length > 0) {
+                connection.release();
+                res.send("The table is not empty. Please clear all data first.");
+
+            } else {
+                // Data array, to be inserted into table via SQL
+                let marketingDataRows = [];
+
+                // Loop through the json file and insert each week's data into the database
+                Object.keys(json_file.marketingData).forEach(key => {
+                    const weekNumber = parseInt(key.split("week")[1]);
+                    const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+                    const webVisitors = json_file.marketingData[key].webVisitors;
+                    const prClippings = json_file.marketingData[key].prClippings;
+
+                    marketingDataRows.push([weekNumber, dateCreated, webVisitors, prClippings]);
+                });
+
+                // Insert into table
+                const sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
+
+                connection.query(sql, [marketingDataRows], (error, results, fields) => {
+                    if (error) {
+                        console.log(error);
+                        return res.send(error);
+                    }
+                }).on("end", () => {
+                    connection.release();
+                    console.log("Inserted original data into marketingdata table & released connection.");
+                    res.redirect("/");
+                })
+            }
+        })
+    });
+}
+
+const marketingData_insertNew = (req, res) => {
+    // If no file uploaded
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).send("No files uploaded")
+    }
+
+    // If the uploaded file is not a JSON file
+    if (req.files.marketingDataJSON.mimetype !== "application/json") {
+        res.status(400).send("You can only upload JSON files.")
+    }
+
+    // If the uploaded JSON file is greater 4mb (the max json file size)
+    if (req.files.marketingDataJSON.size > 4000000) {
+        res.status(400).send("The JSON file needs to be less than 4mb.")
+    }
+
+    // Check if the JSON is valid
+    try {
+        JSON.parse(req.files.marketingDataJSON.data);
+        console.log("Parsed successfully. Valid JSON file.")
+    } catch (error) {
+        return res.send("Invalid JSON! Please go back and upload a valid JSON file.")
+    }
+
+    // The uploaded file
+    const uploadedJSONFile = req.files.marketingDataJSON;
+
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.log(err)
+            connection.release();
+            console.log("Error connecting to the DB. Connection released.")
+            return res.send(err);
+        }
+
+        connection.query('SELECT * FROM marketingdata', (err, rows, fields) => {
+            if (err) {
+                console.log(err)
+                connection.release();
+                console.log("Error connecting to the DB. Connection released.")
+                return res.send(err);
+            }
+
+            // If marketingdata table has data in it, delete it and insert new data.
+            if (rows.length > 0) {
+                console.log("There is already data in the database. Deleting it now...");
+
+                // Delete all the data in marketingdata table
+                connection.query('DELETE FROM marketingdata', (err, rows, fields) => {
+                    if (err) {
+                        console.log(err)
+                        connection.release();
+                        console.log("Error connecting to the DB. Connection released.")
+                        return res.send(err);
+                    }
+                });
+
+                // Read the uploaded JSON file
+                const rawData = uploadedJSONFile.data;
+                let json_file = JSON.parse(rawData);
+
+                // Data array, to be inserted into marketingtable
+                let newMarketingDataRows = [];
+
+                // Loop through the json file and insert each week's data into the database
+                Object.keys(json_file.marketingData).forEach(key => {
+                    const weekNumber = parseInt(key.split("week")[1]);
+                    const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+                    const webVisitors = json_file.marketingData[key].webVisitors;
+                    const prClippings = json_file.marketingData[key].prClippings;
+                    newMarketingDataRows.push([weekNumber, dateCreated, webVisitors, prClippings]);
+                });
+
+                // Insert into table
+                const sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
+
+                connection.query(sql, [newMarketingDataRows], (error, results, fields) => {
+                    if (error) {
+                        console.log(err)
+                        connection.release();
+                        console.log("Error connecting to the DB. Connection released.")
+                        return res.send(err);
+                    }
+                }).on("end", () => {
+                    console.log("Finished inserting the given data into marketingdata table.");
+                    // Release the connection
+                    connection.release();
+                    console.log("Released the connection")
+                    res.redirect("/");
+                })
+
+                // If the marketingdata table is already empty
+            } else {
+                // Read the uploaded JSON file
+                const rawData = uploadedJSONFile.data;
+                let json_file = JSON.parse(rawData);
+
+                // Data array, to be inserted into marketingtable
+                let newMarketingDataRows = [];
+
+                // Loop through the json file and insert each week's data into the database
+                Object.keys(json_file.marketingData).forEach(key => {
+                    const weekNumber = parseInt(key.split("week")[1]);
+                    const dateCreated = new Date(json_file.marketingData[key].dateCreated).toLocaleString();
+                    const webVisitors = json_file.marketingData[key].webVisitors;
+                    const prClippings = json_file.marketingData[key].prClippings;
+                    newMarketingDataRows.push([weekNumber, dateCreated, webVisitors, prClippings]);
+                });
+
+                // Insert into table
+                const sql = "INSERT INTO marketingdata (week_number, date_created, web_visitors, pr_clippings) VALUES ?";
+
+                connection.query(sql, [newMarketingDataRows], (error, results, fields) => {
+                    if (error) {
+                        console.log(err)
+                        connection.release();
+                        console.log("Error connecting to the DB. Connection released.")
+                        return res.send(err);
+                    }
+                }).on("end", () => {
+                    console.log("Finished inserting the given data into marketingdata table.");
+                    // Release the connection
+                    connection.release();
+                    console.log("Released the connection")
+                    res.redirect("/");
+                })
+            }
+        })
+    })
+}
+
+module.exports = {
+    marketingData_index,
+    marketingData_delete,
+    marketingData_insertOriginal,
+    marketingData_insertNew
+};
