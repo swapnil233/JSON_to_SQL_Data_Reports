19c8e64b3c38dd89bfcf8c1f70001f62126368ea
Hasan Iqbal <swapniliqbal@gmail.com>
2022-01-25 01:35:38 -0500

    updated sample sql script to select desired product info

--- db_files/products_data_selection.sql
+++ db_files/products_data_selection.sql
@@ -1,57 +1,44 @@
-USE heroku_06cf010c9c84850;
-
 Select 
-product.product_name as sku, 
-product.barcode, 
-product.parent_sku, 
-product.region_code, 
-product.item_type, 
-product.supplier, 
-product.brand,
-product.variant_name, 
-product.short_description,
-pack_data.pack_type, 
-components.component_name, 	
-components.amount, 
-metric.lmm,
-metric.wmm,
-metric.hmm,
-metric.gwg,
-metric.nwg,
-metric.cbm,
-imperial.lin,
-imperial.win,
-imperial.hin,
-imperial.gwlb,
-imperial.nwlb,
-imperial.cbft, 
-price_data.buy_bomUSD,
-price_data.buy_canadaUSD,
-price_data.buy_franceUSD,
-price_data.buy_hongkongUSD,
-price_data.sell_CAD,
-price_data.sell_USD,
-price_data.sell_GBP,
-price_data.sell_EUR,
-price_data.sell_AUD,
-price_data.sell_NZD,
-price_data.sell_SGD,
-price_data.sell_HKD
+        product.product_name as sku, 
+        product.barcode, 
+        product.parent_sku, 
+        product.region_code, 
+        product.item_type, 
+        product.supplier, 
+        product.brand,
+        product.variant_name, 
+        product.short_description,
+        pack_data.pack_type, 
+        components.component_name, 	
+        components.amount, 
+        metric.lmm,
+        metric.wmm,
+        metric.hmm,
+        metric.gwg,
+        metric.nwg,
+        metric.cbm,
+        imperial.lin,
+        imperial.win,
+        imperial.hin,
+        imperial.gwlb,
+        imperial.nwlb,
+        imperial.cbft, 
+        price_data.buy_bomUSD,
+        price_data.buy_canadaUSD,
+        price_data.buy_franceUSD,
+        price_data.buy_hongkongUSD,
+        price_data.sell_CAD,
+        price_data.sell_USD,
+        price_data.sell_GBP,
+        price_data.sell_EUR,
+        price_data.sell_AUD,
+        price_data.sell_NZD,
+        price_data.sell_SGD,
+        price_data.sell_HKD
 
-FROM product 
-LEFT JOIN pack_data ON product.product_id = pack_data.product_id
-LEFT JOIN components ON product.product_id = components.product_id 
-LEFT JOIN metric ON metric.product_id = product.product_id 
-LEFT JOIN imperial ON imperial.product_id = product.product_id 
-LEFT JOIN price_data ON price_data.product_id = product.product_id
-
-
-
-
-
-
-
-
-
-
-
+        FROM product 
+        LEFT JOIN pack_data ON product.product_id = pack_data.product_id
+        LEFT JOIN components ON product.product_id = components.product_id 
+        LEFT JOIN metric ON metric.metric_id = product.product_id 
+        LEFT JOIN imperial ON imperial.imperial_id = product.product_id 
+        LEFT JOIN price_data ON price_data.price_data_id = product.product_id
